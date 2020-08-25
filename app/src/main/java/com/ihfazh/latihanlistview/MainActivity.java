c158de56d6b2aef77225cd75de61af67708b45d8
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-26 00:19:51 +0700

    gunakan custom adaptor

--- app/src/main/java/com/ihfazh/latihanlistview/MainActivity.java
+++ app/src/main/java/com/ihfazh/latihanlistview/MainActivity.java
@@ -2,12 +2,25 @@ package com.ihfazh.latihanlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] dataName = {"Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin"};
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+
+    private PahlawanAdapter adapter;
+
+    private ArrayList<Pahlawan> pahlawanArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +28,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView = findViewById(R.id.lv_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+        adapter = new PahlawanAdapter(this);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, pahlawanArrayList.get(i).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        prepare();
+        addItems();
+
+
+    }
+
+    private void addItems() {
+        pahlawanArrayList = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++){
+            Pahlawan pahlawan = new Pahlawan();
+            pahlawan.setDescription(dataDescription[i]);
+            pahlawan.setName(dataName[i]);
+            pahlawan.setPhoto(dataPhoto.getResourceId(i, -i));
+            pahlawanArrayList.add(pahlawan);
+        }
+
+        adapter.setPahlawanArrayList(pahlawanArrayList);
+    }
+
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
     }
 }
---
+++ app/src/main/java/com/ihfazh/latihanlistview/PahlawanAdapter.java
@@ -0,0 +1,70 @@
+package com.ihfazh.latihanlistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class PahlawanAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Pahlawan> pahlawanArrayList = new ArrayList<>();
+
+    public PahlawanAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return pahlawanArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return pahlawanArrayList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View itemView = view;
+        if (itemView == null){
+            itemView = LayoutInflater.from(context).inflate(R.layout.item_hero, viewGroup, false);
+        }
+
+        ViewHolder viewHolder = new ViewHolder(itemView);
+        Pahlawan pahlawan = (Pahlawan) getItem(i);
+        viewHolder.bind(pahlawan);
+
+        return itemView;
+    }
+
+    public void setPahlawanArrayList(ArrayList<Pahlawan> pahlawanArrayList) {
+        this.pahlawanArrayList = pahlawanArrayList;
+    }
+
+    private class ViewHolder {
+        private ImageView photo;
+        private TextView name, description;
+
+        public ViewHolder(View itemView) {
+            photo = itemView.findViewById(R.id.img_hero);
+            name = itemView.findViewById(R.id.txt_name);
+            description = itemView.findViewById(R.id.txt_description);
+        }
+
+        public void bind(Pahlawan pahlawan) {
+            photo.setImageResource(pahlawan.getPhoto());
+            name.setText(pahlawan.getName());
+            description.setText(pahlawan.getDescription());
+        }
+    }
+}

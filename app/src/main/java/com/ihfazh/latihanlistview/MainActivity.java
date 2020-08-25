3c41f1bece43d1ab91ad6605cb806ab1c528d5b9
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-25 23:50:28 +0700

    simple list view

--- app/src/main/java/com/ihfazh/latihanlistview/MainActivity.java
+++ app/src/main/java/com/ihfazh/latihanlistview/MainActivity.java
@@ -3,12 +3,19 @@ package com.ihfazh.latihanlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] dataName = {"Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.lv_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+        listView.setAdapter(adapter);
     }
 }
